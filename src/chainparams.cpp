2de24c77f10eb71a0704b0a049f50c238c2f7b17
mbambnag <38546495+mbambnag@users.noreply.github.com>
2019-03-04 01:31:44 +0700

    Update Checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -60,6 +60,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     (0, uint256("0x00000c3e7814dac5cc0e41b2049dbed4b50809cca911c0e216639ce4e9d46823"))
     (47274, uint256("0xf4d0e3e70e368d8f31fadab004578da2889d2d526cb27224c789d7a389a5a83e"))
     (128982, uint256("0x0b23d9e3c7aff21ef5d61afd2ed7daab0378274a886cb2ca4a40b02db75dccdc"))
+    (182663, uint256("0x93eb2dc632d831a121983ccbcd70118b5fbed24fded0d54902af6f7c45eae6d5"))
     ;
 
 static const Checkpoints::CCheckpointData data = {
